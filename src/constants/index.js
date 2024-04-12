090a5a33f14c8c3974cb79837d02f213afab14f5
Emmanuel <preiraclement79@gmail.com>
2024-04-12 02:47:39 +0000

    update

--- src/constants/index.js
+++ src/constants/index.js
@@ -28,11 +28,11 @@ import {
 export const navLinks = [
   {
     id: "about",
-    title: "About",
+    title: "À propos",
   },
   {
     id: "work",
-    title: "Work",
+    title: "Projets",
   },
   {
     id: "contact",

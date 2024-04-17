c74ff071c98ce124903e10540f16b0e46bac2f1f
Emmanuel <preiraclement79@gmail.com>
2024-04-17 05:51:29 +0100

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ import { styles } from "../styles";
 import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
-import    maphoto from "../assets/maphoto.jpeg"
+import maphoto from "../assets/maphoto.jpeg";
 
 const ServiceCard = ({ index, title, icon }) => (
   <Tilt className='xs:w-[250px] w-full'>
